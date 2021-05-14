078ba361ba49c59428d49cd8947fb1c3790b7416
John Strain <jjstrain68@gmail.com>
2021-05-14 14:55:09 +0100

    Added brute force and set the time and space complexities

--- src/BigO/example/FirstRecurring.java
+++ src/BigO/example/FirstRecurring.java
@@ -6,8 +6,8 @@ import java.util.HashSet;
 public class FirstRecurring {
 
     public static void main(String[] args) {
-        int[] arr = {2, 5, 5, 2, 3, 5, 1, 2, 4};
-        //int[] arr = {2,1,1,2,3,5,1,2,4}; //It should return 1
+        //int[] arr = {2, 5, 5, 2, 3, 5, 1, 2, 4};
+        int[] arr = {2,1,1,2,3,5,1,2,4}; //It should return 1
         //int[] arr = {2,3,4,5}; //It should return null
         //int[] arr = {2,5,5,2,3,5,1,2,4}; //return 5 because the pairs are before 2,2
         FirstRecurring obj = new FirstRecurring();
@@ -18,14 +18,29 @@ public class FirstRecurring {
             System.out.println("No recurring element found in array");
     }
 
+    private Integer firstRecurringElementBruteForce(int[] array){
+
+        //Time Complexity = O(i*j)
+        //Space Complexity = O(i)
+        for (int i=1; i < array.length; i++) {
+            for (int j=i-1; j >= 0; j--) {
+                if (array[i] == array[j])
+                    return array[i];
+            }
+        }
+        return null;
+    }
+
     private Integer firstRecurringElement(int[] array){
         HashSet<Integer> set = new HashSet<>();
 
-        for (int j : array) {
-            if (set.contains(j))
-                return j;
+        //Space complexity O(i)
+        //Time complexity O(i+i)
+        for (int i : array) {
+            if (set.contains(i))
+                return i;
             else
-                set.add(j);
+                set.add(i);
         }
 
         return null;
