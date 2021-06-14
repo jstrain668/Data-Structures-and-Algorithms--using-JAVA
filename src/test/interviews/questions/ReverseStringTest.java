1d783b7ecf064dc94a6f27535f739d768e8a350b
John Strain <jjstrain68@gmail.com>
2021-06-14 15:14:03 +0100

    ReverseString test cases

---
+++ src/test/interviews/questions/ReverseStringTest.java
@@ -0,0 +1,40 @@
+package interviews.questions;
+
+
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReverseStringTest {
+
+    ReverseString rs;
+
+    @BeforeEach
+    public void init(){
+        rs = new ReverseString();
+    }
+
+    @Test
+    void reverseString() {
+        assertEquals("", rs.reverseString(""));
+    }
+
+    @Test public void reverseNullString(){
+        assertEquals(null, rs.reverseString(null));
+    }
+
+    @Test public void reversePalindromeString(){
+        assertEquals("aba", rs.reverseString("aba"));
+    }
+
+    @Test public void reverseSameCharacterString(){
+        assertEquals("aaa", rs.reverseString("aaa"));
+    }
+
+    @Test public void reverseAnagramString(){
+        assertEquals("mary", rs.reverseString("yram"));
+    }
+}
