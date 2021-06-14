9893ebc775529f91dfb87e1048365bc69e257a38
John Strain <jjstrain68@gmail.com>
2021-06-14 19:05:01 +0100

    Added FizzBuzz answer

---
+++ src/interviews/questions/FizzBuzz.java
@@ -0,0 +1,43 @@
+package interviews.questions;
+
+import java.util.*;
+
+public class FizzBuzz {
+
+    // Description: Loop from 1 to n, diving each value to find number is divisible by 3 and 5, 3 only, 5 only and not satisfying
+    // any of the above conditions. Add the string for each case to ArrayList. When adding the index starts from zero, thus the decrement of 1.
+    // Time Complexity: O(n)
+    // Space complexity of O(n)
+    public List<String> fizzBuzz(int n){
+
+        if (n <=0)
+            return Collections.emptyList();
+
+        List<String> strings = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++){
+
+            if (i % 3 == 0 && i % 5 == 0)
+                strings.add(i-1,"FizzBuzz");
+            else if (i % 3 == 0)
+                strings.add(i-1, "Fizz");
+            else if (i % 5 == 0)
+                strings.add(i-1,"Buzz");
+            else
+                strings.add(i-1,String.valueOf(i));
+        }
+
+        return strings;
+    }
+
+    public static void main(String[] args) {
+        FizzBuzz fb = new FizzBuzz();
+        int n = 15;
+
+        System.out.println("Integer number "+n);
+        List<String> strings = fb.fizzBuzz(n);
+        for (String str: strings){
+            System.out.println(str);
+        }
+    }
+}
