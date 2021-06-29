ae8ef92ecbf8c471e3e60ce927d156010105f62c
John Strain <jjstrain68@gmail.com>
2021-06-29 11:23:59 +0100

    Added TwoSum test cases

---
+++ src/test/interviews/questions/TwoSumTest.java
@@ -0,0 +1,29 @@
+package interviews.questions;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoSumTest {
+
+    TwoSum ts;
+    int[] nums1;
+    int[] nums2;
+    int[] nums3;
+
+    @BeforeEach
+    void init(){
+        ts = new TwoSum();
+        nums1 = new int[] {2,7,11,15};
+        nums2 = new int[] {3,2,4};
+        nums3 = new int[] {3,3};
+    }
+
+    @Test
+    void twoSum() {
+        assertArrayEquals(new int[] {1,0}, ts.twoSum(nums1,9));
+        assertArrayEquals(new int[] {2,1}, ts.twoSum(nums2,6));
+        assertArrayEquals(new int[] {0,1}, ts.twoSum(nums3,6));
+    }
+}
