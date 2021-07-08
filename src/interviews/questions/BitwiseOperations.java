c8b441953e705fa11ee69f5bb99d3e3c95314365
John Strain <jjstrain68@gmail.com>
2021-07-08 13:24:15 +0100

    Added multiply

--- src/interviews/questions/BitwiseOperations.java
+++ src/interviews/questions/BitwiseOperations.java
@@ -48,6 +48,26 @@ public class BitwiseOperations {
         return x;
     }
 
+    public int multiply(int n, int m)
+    {
+        int ans = 0, count = 0;
+        while (m > 0)
+        {
+            // check for set bit and left
+            // shift n, count times
+            if (m % 2 == 1)
+                ans += n << count;
+
+            // increment of place
+            // value (count)
+            count++;
+            m /= 2;
+        }
+
+        return ans;
+    }
+
+
     /**
      * Sets a specific bit of an int.
      *
