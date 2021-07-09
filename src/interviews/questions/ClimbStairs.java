949263fd04703970e68cb4a34fc65ec9a576d1bc
John Strain <jjstrain68@gmail.com>
2021-07-09 19:16:56 +0100

    Added Leetcode question reference

--- src/interviews/questions/ClimbStairs.java
+++ src/interviews/questions/ClimbStairs.java
@@ -1,12 +1,12 @@
 package interviews.questions;
 
-import java.util.HashMap;
+//Reference; https://leetcode.com/problems/climbing-stairs/
 
 public class ClimbStairs {
 
     //Description: Problem similar to the Fibonacci sequence. When n is i, the result is the sum of
     // i-1 and i-2. The simplest is top-down recursive solution, but the time overhead is relatively large
-    // Time Complexity: O(2to the poser of n)
+    // Time Complexity: O(2to the power of n)
     //Space Complexity: O(1)
     public int climbStairs(int n){
 
@@ -47,9 +47,66 @@ public class ClimbStairs {
         return cache[n-1];
     }
 
+    private int climbStairsBF(int i, int n){
+        if (i > n){
+            return 0;
+        }
+
+        if (i==n){
+            return 1;
+        }
+        return climbStairsBF(i+1,n) + climbStairsBF(i+2,n);
+    }
+
+    public int climbStairsBF(int n){
+        if (n < 1 || n > 45){
+            throw new IllegalArgumentException("Invalid number passed");
+        }
+
+        if (n ==1 || n == 2){
+            return n;
+        }
+
+        return climbStairsBF(0,n);
+    }
+    private int climbStairsBFWithCache(int i, int n, int[] cache){
+        if (i > n){
+            return 0;
+        }
+
+        if (i==n){
+            return 1;
+        }
+
+        if (cache[i] > 0){
+            return cache[i];
+        }
+
+        cache[i] = climbStairsBFWithCache(i+1,n,cache) + climbStairsBFWithCache(i+2,n,cache);
+        return cache[i];
+    }
+
+    //Time Complexity: O(2 power of n)
+    //Space Complexity: O(n)
+    public int climbStairsBFWithCache(int n){
+
+        if (n < 1 || n > 45){
+            throw new IllegalArgumentException("Invalid number passed");
+        }
+
+        if (n ==1 || n == 2){
+            return n;
+        }
+
+        int[] cache = new int[n+1];
+        return climbStairsBFWithCache(0,n, cache);
+    }
+
     public static void main(String[] args) {
         ClimbStairs cs = new ClimbStairs();
         int n = 5;
+        System.out.println("Number of possible steps to climb stairs: "+cs.climbStairsBF(n));
+        System.out.println("Number of possible steps to climb stairs: "+cs.climbStairsBFWithCache(n));
         System.out.println("Number of possible steps to climb stairs: "+cs.climbStairs(n));
         System.out.println("Number of possible steps to climb stairs: "+cs.climbStairsUsingCache(n));
     }
