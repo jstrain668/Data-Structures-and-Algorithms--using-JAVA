2d93e779364101e28a5228fbeaf5ff9afd06d972
John Strain <jjstrain68@gmail.com>
2021-07-09 10:53:54 +0100

    Added LeetCode interview code reference

--- src/interviews/questions/ContainsDuplicate.java
+++ src/interviews/questions/ContainsDuplicate.java
@@ -4,11 +4,37 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 
+//Description: https://leetcode.com/problems/contains-duplicate/
+
 public class ContainsDuplicate {
+
+    //Description: Sort the numbers, so that a linear loop comparison can be performed to determine
+    //if there is a duplicate or not. After sorting, duplicates will be next to each other.
+    //Time Complexity: O(n logn) for the sort and O(n) for the loop. O(n logn) is the dominant term
+    //Space Complexity: O(1)
+
+    public boolean containsDuplicate2(int[] nums){
+
+        if (nums==null || nums.length==0)
+            throw new IllegalArgumentException("Null or empty array not allowed");
+
+        if (nums.length == 1)
+            return false;
+
+        Arrays.sort(nums);
+
+        for (int i=1; i < nums.length; i++){
+            if (nums[i-1] == nums[i]){
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     //Description: Use HashSet, to detect duplicates when adding values to the HashSet. If it
     //already contains the element already return true otherwise false.
-    // Time Complexity: O(n)
+    // Time Complexity: O(n). Note constant time performance for the add and contains functions
     // Space Complexity: O(n)
     public boolean containsDuplicate(int[] nums){
 
