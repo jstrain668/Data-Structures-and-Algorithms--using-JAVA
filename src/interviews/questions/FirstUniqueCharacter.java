abf1a2b5d76a2ddf52eb99bc21b5b9673190d1c9
John Strain <jjstrain68@gmail.com>
2021-07-09 11:15:13 +0100

    Added LeetCode interview code reference

--- src/interviews/questions/FirstUniqueCharacter.java
+++ src/interviews/questions/FirstUniqueCharacter.java
@@ -2,12 +2,14 @@ package interviews.questions;
 
 import java.util.HashMap;
 
+//Reference: https://leetcode.com/problems/first-unique-character-in-a-string/
+
 public class FirstUniqueCharacter {
 
     // Description: To find first unique character process the string, recording the count of each
     // character in a HasMap. Loop through the string again, returning the index of the string for the
     // first occurrence of a character in the hash map with a count of 1
-    // Time Complexity: O(n+n)
+    // Time Complexity: O(n+n) =O(n)
     // Space Complexity: O(n)
 
     public int firstUniqueCharWithHashMap(String s){
@@ -32,6 +34,27 @@ public class FirstUniqueCharacter {
         return -1;
     }
 
+    public int firstUniqChar(String s) {
+        int[] count = new int[26];
+        int n = s.length();
+        // build char count bucket : <charIndex, count>
+        for (int i = 0; i < n; i++) {
+            int index = s.charAt(i) - 'a';
+            count[index]++;
+        }
+
+        // find the index
+        for (int i = 0; i < n; i++) {
+            int index = s.charAt(i) - 'a';
+            if (count[index] == 1) {
+                return i;
+            }
+
+        }
+        return -1;
+    }
+
+
     //Description: Find the index of the first character which shares the same index value for
     // the first occurrence check and last occurrence check against the supplied string. Return index if
     // found otherwise -1 if none found.
@@ -55,6 +78,7 @@ public class FirstUniqueCharacter {
         System.out.println("Input string: "+str2);
         System.out.println("First unique character: "+fuc.firstUniqueCharWithHashMap(str2));
         System.out.println("First unique character: "+fuc.firstUniqueChar(str2));
+        System.out.println("First unique character: "+fuc.firstUniqChar(str2));
 
     }
 }
