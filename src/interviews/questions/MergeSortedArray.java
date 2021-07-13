719510b5ff59686247df30147bf5d4b59593689d
John Strain <jjstrain68@gmail.com>
2021-07-13 14:21:39 +0100

    Added Leetcode question reference

--- src/interviews/questions/MergeSortedArray.java
+++ src/interviews/questions/MergeSortedArray.java
@@ -2,10 +2,12 @@ package interviews.questions;
 
 import java.util.Arrays;
 
+//Description: https://leetcode.com/problems/merge-sorted-array/
+
 public class MergeSortedArray {
 
     //Description: Append elements from nums2 to nums1 and sort the resultant array
-    //Time Complexity: O(n) + O(n log n)
+    //Time Complexity: O(n logn) = O(n) + O(n log n)
     //Space Complexity: O(1)
     public void simpleMerge(int[] nums1, int m, int[] nums2, int n) {
 
@@ -20,6 +22,35 @@ public class MergeSortedArray {
         }
 
         Arrays.sort(nums1);
+    }
+
+    //Description: Merge into the end of nums1 array, inserting largest value from both arrays into
+    //last position and continue on comparing between the source arrays and checking if m/n are less
+    //than zero
+    //Time Complexity: O(n)
+    //Space Complexity: O(1)
+    public void merge2(int[] nums1, int m, int[] nums2, int n) {
+
+        int totalLen = m + n;
+        m--;
+        n--;
+
+        for(int i = totalLen - 1; i >= 0; i--){
+            if(m < 0){
+                nums1[i] = nums2[n];
+                n--;
+            } else if (n < 0){
+                nums1[i] = nums1[m];
+                m--;
+            } else if(nums1[m] <= nums2[n]){
+                nums1[i] = nums2[n];
+                n--;
+            } else {
+                nums1[i] = nums1[m];
+                m--;
+            }
+        }
+
     }
 
     //Description: Starting from the end of each array, insert largest element at the end of nums1.
@@ -57,7 +88,7 @@ public class MergeSortedArray {
         System.out.println("Nums1 : "+ Arrays.toString(nums1));
         System.out.println("Nums2 : "+ Arrays.toString(nums2));
         //msa.simpleMerge(nums1, 0, nums2, nums2.length);
-        msa.merge(nums1,3,nums2,nums2.length);
+        msa.merge2(nums1,3,nums2,nums2.length);
         System.out.println("Nums1 : "+ Arrays.toString(nums1));
 
     }
