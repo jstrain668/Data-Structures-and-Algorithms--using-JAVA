94075a7f2bdb0c5666909b524798311d441568d2
John Strain <jjstrain68@gmail.com>
2021-08-18 12:22:11 +0100

    Updated edge case in naiveLengthOfLIS and lengthOfLIS

--- src/interviews/questions/amazon/LongestIncreasingSubsequence.java
+++ src/interviews/questions/amazon/LongestIncreasingSubsequence.java
@@ -18,7 +18,7 @@ public class LongestIncreasingSubsequence {
     public int naiveLengthOfLIS(int[] nums) {
 
         if (nums == null || nums.length == 0){
-            return -1;
+            return 0;
         }
 
         if (nums.length == 1){
@@ -103,8 +103,8 @@ public class LongestIncreasingSubsequence {
     // Space Complexity: O(n) for the ArrayList
 
     public int lengthOfLIS2(int[] nums) {
-        if (nums == null) {
-            return -1;
+        if (nums == null || nums.length == 0) {
+            return 0;
         }
 
         if (nums.length == 1){
